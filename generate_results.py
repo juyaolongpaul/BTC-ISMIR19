1fb30447f6c9d2408d32e63754cb7d170a281c4a
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2020-11-16 11:51:49 +0800

    Save audio features as well

--- generate_results.py
+++ generate_results.py
@@ -4,6 +4,7 @@ import pretty_midi as pm
 from utils import logger
 from btc_model import *
 from utils.mir_eval_modules import audio_file_to_features, idx2chord, idx2voca_chord, get_audio_paths_with_id, get_audio_paths
+from multiprocessing import Pool, Process
 import argparse
 import warnings
 import pandas as pd
@@ -12,58 +13,31 @@ warnings.filterwarnings('ignore')
 logger.logging_verbosity(1)
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
-if __name__ == '__main__':
-# hyperparameters
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--voca', default=True, type=lambda x: (str(x).lower() == 'true'))
-    parser.add_argument('--audio_dir', type=str, default='./test')
-    parser.add_argument('--src_csv', type=str, help='source csv path with song id', default='empty')
-    parser.add_argument('--save_label_one_hot_dir', type=str, default='./test')
-    parser.add_argument('--save_dir', type=str, default='./test')
-    parser.add_argument('--save_attention_feature_dir', type=str, default='./test')
-    parser.add_argument('--sep', type=str, default='\t', help='sep')
-    parser.add_argument('--reextract', type=str, default='N', help='specify whether to re-extract features')
-    args = parser.parse_args()
 
-    config = HParams.load("run_config.yaml")
-    if args.src_csv != 'empty':
-        df = pd.read_csv(args.src_csv, index_col=False, sep=args.sep)
-        songids = list(df['songid'].apply(str))  # obtain all the songs specified in csv that need to extract features
-        audio_paths = get_audio_paths_with_id(args.audio_dir, songids)
-    else:  # if not specified, list all the .wav and .mp3 files
-        audio_paths = get_audio_paths(args.audio_dir)
-    if args.voca is True:
-        config.feature['large_voca'] = True
-        config.model['num_chords'] = 170
-        model_file = './test/btc_model_large_voca.pt'
-        idx_to_chord = idx2voca_chord()
-        logger.info("label type: large voca")
-    else:
-        model_file = './test/btc_model.pt'
-        idx_to_chord = idx2chord
-        logger.info("label type: Major and minor")
-
-    model = BTC_model(config=config.model).to(device)
-
-    # Load model
-    if os.path.isfile(model_file):
-        checkpoint = torch.load(model_file, map_location='cpu')
-        mean = checkpoint['mean']
-        std = checkpoint['std']
-        model.load_state_dict(checkpoint['model'])
-        logger.info("restore model")
-
-
+def extract_chords_features(args, audio_path, i):
 
     # Chord recognition and save lab file
-    for i, audio_path in enumerate(audio_paths):
-        logger.info("======== %d of %d in progress ========" % (i + 1, len(audio_paths)))
-        # Load mp3
-        if os.path.isfile(os.path.join(args.save_label_one_hot_dir,
-                                 os.path.split(audio_path)[-1].replace('.mp3', '').replace('.wav', '') + '_predicted_one_hot_label.npy')) \
-                          and os.path.isfile(os.path.join(args.save_attention_feature_dir, os.path.split(audio_path)[-1].replace('.mp3', '').replace('.wav', '') + '_attention_layer_output.npy')) and args.reextract == 'N':
-            continue  # if feature already extracted, and unless specified to re-extract features, skip this file
-        feature, feature_per_second, song_length_second = audio_file_to_features(audio_path, config)
+    logger.info("======== %d of %d in progress for feature extraction ========" % (i + 1, len(audio_paths)))
+    # Load mp3
+    if not (os.path.isfile(os.path.join(args.save_label_one_hot_dir,
+                                   os.path.split(audio_path)[-1].replace('.mp3', '').replace('.wav',
+                                                                                             '') + '_predicted_one_hot_label.npy')) \
+            and os.path.isfile(os.path.join(args.save_attention_feature_dir,
+                                            os.path.split(audio_path)[-1].replace('.mp3', '').replace('.wav',
+                                                                                                      '') + '_attention_layer_output.npy')) and args.reextract == 'N'):
+        # if feature already extracted, and unless specified to re-extract features, skip this file
+        if not os.path.exists(args.save_audio_feature):
+            os.makedirs(args.save_audio_feature)
+        audio_feature_path = os.path.join(args.save_audio_feature,
+                                          os.path.split(audio_path)[-1].replace('.mp3', '').replace('.wav',
+                                                                                                    '') + '_chord_audio_feature.npy')
+        if not os.path.isfile(audio_feature_path):
+            feature, feature_per_second, song_length_second = audio_file_to_features(audio_path, config)
+            with open(audio_feature_path, 'wb') as f_audio_feature:
+                np.save(f_audio_feature, feature)
+        else:
+            feature = np.load(audio_feature_path)
+            feature_per_second = config.mp3['inst_len'] / config.model['timestep']
         logger.info("audio file loaded and feature computation success : %s" % audio_path)
 
         # Majmin type chord recognition
@@ -133,6 +107,7 @@ if __name__ == '__main__':
 
         logger.info("label file saved : %s" % save_path)
         ## output feature
+
         feature_path_predicted_label = os.path.join(args.save_label_one_hot_dir,
                                  os.path.split(audio_path)[-1].replace('.mp3', '').replace('.wav', '') + '_predicted_one_hot_label.npy')
         with open(feature_path_predicted_label, 'wb') as f_predicted_label:
@@ -144,3 +119,62 @@ if __name__ == '__main__':
         with open(feature_path_attention_output, 'wb') as f_attention_output:
             np.save(f_attention_output, a_self_attn_output)
         logger.info("attention embedding features saved : %s" % feature_path_attention_output)
+
+
+if __name__ == '__main__':
+# hyperparameters
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--voca', default=True, type=lambda x: (str(x).lower() == 'true'))
+    parser.add_argument('--audio_dir', type=str, default='./test')
+    parser.add_argument('--src_csv', type=str, help='source csv path with song id', default='empty')
+    parser.add_argument('--save_label_one_hot_dir', type=str, default='./test')
+    parser.add_argument('--save_dir', type=str, default='./test')
+    parser.add_argument('--save_attention_feature_dir', type=str, default='./test')
+    parser.add_argument('--save_audio_feature', type=str, default='./test')
+    parser.add_argument('--sep', type=str, default='\t', help='sep')
+    parser.add_argument('--need_multithreads', type=str, default='N', help='specify whether multi-threading is needed')
+    parser.add_argument('--n_thread', type=int, default=8, help='thread number')
+    parser.add_argument('--reextract', type=str, default='N', help='specify whether to re-extract features')
+    args = parser.parse_args()
+
+    config = HParams.load("run_config.yaml")
+    if args.src_csv != 'empty':
+        df = pd.read_csv(args.src_csv, index_col=False, sep=args.sep)
+        songids = list(df['songid'].apply(str))  # obtain all the songs specified in csv that need to extract features
+        audio_paths = get_audio_paths_with_id(args.audio_dir, songids)
+    else:  # if not specified, list all the .wav and .mp3 files
+        audio_paths = get_audio_paths(args.audio_dir)
+    if args.voca is True:
+        config.feature['large_voca'] = True
+        config.model['num_chords'] = 170
+        model_file = './test/btc_model_large_voca.pt'
+        idx_to_chord = idx2voca_chord()
+        logger.info("label type: large voca")
+    else:
+        model_file = './test/btc_model.pt'
+        idx_to_chord = idx2chord
+        logger.info("label type: Major and minor")
+
+    model = BTC_model(config=config.model).to(device)
+
+    # Load model
+    if os.path.isfile(model_file):
+        checkpoint = torch.load(model_file, map_location='cpu')
+        mean = checkpoint['mean']
+        std = checkpoint['std']
+        model.load_state_dict(checkpoint['model'])
+        logger.info("restore model")
+    if args.need_multithreads == 'Y':
+        jobs = []
+        pool = Pool(args.n_thread)
+        for i, audio_path in enumerate(audio_paths):
+            p = Process(target=extract_chords_features, args=(args, audio_path, i))
+            jobs.append(p)
+            p.start()
+            # pool.apply_async(extract_chords, args=(args, audio_path, i))
+        # pool.close()
+        # pool.join()
+    else:
+        for i, audio_path in enumerate(audio_paths):
+            extract_chords_features(args, audio_path, i)
+
